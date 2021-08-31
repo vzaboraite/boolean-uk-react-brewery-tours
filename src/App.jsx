160996e3c4c61d311bcc0623c24d4f96fcd84c22
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-08-31 15:29:09 +0100

    Create `handler` functions; do `fetch` call; save data in `state`.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 
 export default function App() {
+  /* STATE */
+  const [selectState, setSelectState] = useState("");
+  const [breweries, setBreweries] = useState([]);
+  const [cities, setCities] = useState([]);
+
+  console.log("State: ", {
+    selectState,
+    breweries,
+    cities,
+  });
+
+  /* API */
+
+  const getBreweriesByState = (selectedState) =>
+    fetch(
+      `https://api.openbrewerydb.org/breweries?by_state=${selectedState}&per_page=50`
+    )
+      .then((res) => res.json())
+      .then((breweriesData) => {
+        // console.log("breweriesData inside selectState fetch: ", breweriesData);
+        setBreweries(breweriesData);
+      });
+
+  /* City list extracted from breweries state to use in `FilterSection` cities checkboxes */
+  useEffect(() => {
+    const extractedCities = breweries.map((brewery) => brewery.city);
+    const uniqueCities = [...new Set(extractedCities)].sort();
+    setCities(uniqueCities);
+  }, [breweries]);
+
+  /*  HANDLER FUNCTIONS */
+  /* Header Section */
+  const handleSelectStateInput = (event) => {
+    // console.log("Inside handleSelectStateInput: ", event.target.value);
+    setSelectState(event.target.value);
+  };
+
+  const handleSelectStateSubmit = (event) => {
+    event.preventDefault();
+    // console.log("Inside handleSelectStateSubmit: ", event.target);
+    getBreweriesByState(selectState);
+  };
+
+  /* COMPONENT */
   return (
     <>
-      <Header />
+      <Header
+        selectState={selectState}
+        onChange={handleSelectStateInput}
+        onSubmit={handleSelectStateSubmit}
+      />
       <main>
         {/* FILTER SECTION */}
         <aside className="filters-section">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-export default function Header() {
+export default function Header({ selectState, onChange, onSubmit }) {
   return (
     <header className="main-header">
       <section className="select-state-section">
         <h2>Welcome to Brewery Tours</h2>
-        <form id="select-state-form" autoComplete="off">
+        <form id="select-state-form" autoComplete="off" onSubmit={onSubmit}>
           <label htmlFor="select-state">Which state are you visiting?</label>
-          <input id="select-state" name="select-state" type="text" />
+          <input
+            id="select-state"
+            name="select-state"
+            type="text"
+            value={selectState}
+            onChange={onChange}
+          />
         </form>
       </section>
     </header>
